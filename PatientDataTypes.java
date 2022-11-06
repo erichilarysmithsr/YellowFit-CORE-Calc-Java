22876957da50a4aa3245b99a5f3e6904e4b4e6d5
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2022-11-06 17:49:58 -0600

    Add files via upload
    Added new PatientDataTypes.java file to main

---
+++ PatientDataTypes.java
@@ -0,0 +1,16 @@
+class PatientDataTypes
+  { 
+    public static void main ( String[] args ) {
+      char letter = 'P';
+        String title = "Patient Data Types";
+      int number = 70; 
+        float decimal = 98.6f;
+      boolean result = true;
+      System.out.println("Patient is " + letter ) ;
+      System.out.println("Project is " + title ) ;
+      System.out.println("Patient weight is " + number ) ;
+      System.out.println("Patient temperature is " + decimal ) ;
+      System.out.println("Answer is " + result ) ;
+        
+    }
+  }
